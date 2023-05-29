2fa699287cc4ff718ba92a5bc5468e04c14dece3
snevr <s.nevrovsky@ya.ru>
2023-05-29 20:49:49 +0300

    )

--- main.py
+++ main.py
@@ -7,16 +7,23 @@ from effects import *
 class App:
     def __init__(self, master):
         self.master = master
-        self.master.title("Image filters")
+        self.master.title("FOTOHREN")
 
-        self.original_image = Image.open(filedialog.askopenfilename())
-        self.filtered_image = self.original_image.copy()
+        self.filtered_image = Image.open(filedialog.askopenfilename(filetypes=(("JPG", "*.jpg"),("JPEG", "*.jpeg"))))
 
         self.img_label = tk.Label(self.master)
         self.img_label.pack()
         self.update_image()
 
-        self.filters = [{"name": "Grayscale", "func": self.grayscale_filter}]
+        self.filters = [{"name": "Grayscale", "func": self.grayscale_filter},
+                        {"name": "Narko", "func": self.narko_filter},
+                        {"name": "Noise", "func": self.noise_filter},
+                        {"name": "Color_noise", "func": self.color_noise_filter},
+                        {"name": "Hetvertirovanie", "func": self.hetvertirovanie_filter},
+                        {"name": "Kek", "func": self.kek_filter},
+                        {"name": "Razm", "func": self.razm_filter},
+                        {"name": "Rezk", "func": self.rezk_filter},
+                        {"name": "Save", "func": self.save}]
         for f in self.filters:
             filter_button = tk.Button(self.master, text=f["name"], command=f["func"])
             filter_button.pack()
@@ -30,6 +37,35 @@ class App:
         self.filtered_image = blasck_white(self.filtered_image)
         self.update_image()
 
+    def narko_filter(self):
+        self.filtered_image = gren_narko(self.filtered_image)
+        self.update_image()
+
+    def noise_filter(self):
+        self.filtered_image = noise(self.filtered_image)
+        self.update_image()
+
+    def color_noise_filter(self):
+        self.filtered_image = color_noise(self.filtered_image)
+        self.update_image()
+
+    def hetvertirovanie_filter(self):
+        self.filtered_image = hetvertirovanie(self.filtered_image)
+        self.update_image()
+    def kek_filter(self):
+        self.filtered_image = kek(self.filtered_image)
+        self.update_image()
+    def razm_filter(self):
+        self.filtered_image = razm(self.filtered_image)
+        self.update_image()
+    def rezk_filter(self):
+        self.filtered_image = kek(self.filtered_image)
+        self.update_image()
+    def save(self):
+        path = filedialog.asksaveasfilename()
+        path = path+".jpg"
+        print(path)
+        self.filtered_image.save(path)
 
 root = tk.Tk()
 app = App(root)
